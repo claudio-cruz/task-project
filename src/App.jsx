4dac13d0454ac5792662f72672d54a3f4af03a53
Claudio Cruz <claudiocruz1989@gmail.com>
2024-01-02 19:39:18 +0000

    Create NewProject.jsx and Input.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from "./Components/NewProject";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-    </>
+      <NewProject />
+    </main>
   );
 }
 
