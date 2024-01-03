3295d73e7723ebc177e83d7b7e0fe1d02877b3ac
Claudio Cruz <claudiocruz1989@gmail.com>
2024-01-03 19:20:35 +0000

    Add handleAddProject to app.jsx

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from './Input'
 
-function NewProject() {
+function NewProject({onAdd}) {
+  const title = useRef()
+  const description = useRef()
+  const dueDate = useRef()
+
+  function handleSave() {
+    const enteredTitle = title.current.value
+    const enteredDescription = description.current.value
+    const enteredDueDate = dueDate.current.value
+
+    // validation...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    })
+  }
+
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
@@ -13,15 +31,16 @@ function NewProject() {
         <li>
           <button
             className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
-            >
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input type='text' ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type='date' ref={dueDate} label="Due Date" />
       </div>
     </div>
   )
